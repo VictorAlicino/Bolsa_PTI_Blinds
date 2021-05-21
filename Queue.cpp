13ae788686dcd896168209ef226cee3c05df622f
Victor <victor.alicino@gmail.com>
2021-05-21 18:11:47 -0300

    A segunda batata

--- Queue.cpp
+++ Queue.cpp
@@ -1,3 +1,7 @@
 #include "Queue.h"
 
-Queue::Queue
+Queue_Node::Queue_Node(T msg){
+    this->message = msg;
+    this->next = nullptr;
+}
+
--- Queue.h
+++ Queue.h
@@ -1,15 +1,32 @@
 #ifndef QUEUE_H
 #define QUEUE
 
+template <class T>
+class Queue_Node{
+private:
+    T message;
+    Queue_Node *next;
+public:
+    Queue_Node(T msg);
+    template <class T> T getMessage();
+    ~Queue_Node();
+};
 
-template <typename T>
+template <class T>
 class Queue{
 private:
     T messages;
-        
+    Queue_Node<T> queue;
+    Queue_Node<T> *queue_back;
 public:
-    Queue();     // constructor
-    ~Queue();                   // destructor
+    Queue();
+    ~Queue();
+    bool empty();
+    int size();
+    T front();
+    T back();
+    void push(T msg);
+    void pop();
 };
 
 #endif
