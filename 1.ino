01b88d3afcb724f029e5073b3fa9216ff8ebf673
Victor <victor.alicino@gmail.com>
2021-05-21 17:13:13 -0300

    The First Potato

---
+++ 1.ino
@@ -0,0 +1,8 @@
+#define led_pin 1
+
+void setup(){
+    pinMode(led_pin, INPUT);
+}
+
+void loop(){
+}
---
+++ Queue.h
@@ -0,0 +1,15 @@
+#ifndef QUEUE_H
+#define QUEUE
+
+
+template <typename T>
+class Queue{
+private:
+    T messages;
+        
+public:
+    Queue();     // constructor
+    ~Queue();                   // destructor
+};
+
+#endif
