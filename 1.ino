fd66239fe7e6a858ca70f09f34f8088e9104dfec
VictorAlicino <victor.alicino@gmail.com>
2021-05-28 14:54:39 -0300

    batata

--- 1.ino
+++ 1.ino
@@ -1,7 +1,11 @@
-#define led_pin 1
+#define BUTTON_01   2 
+#define BUTTON_02   3
+#define BUTTON_03   4
 
 void setup(){
-    pinMode(led_pin, INPUT);
+    pinMode(BUTTON_01, INPUT);
+    pinMode(BUTTON_02, INPUT);
+    pinMode(BUTTON_03, INPUT);
 }
 
 void loop(){
